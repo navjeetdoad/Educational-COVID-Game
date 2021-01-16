90367a31a2ff7da7505f88b04f9f1470eefd6a0f
Navjeet Doad <46533161+navjeetdoad@users.noreply.github.com>
2021-01-16 12:24:16 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -5,7 +5,25 @@ from BoyAI import *
 from GirlAI import *
 
 screen = display.set_mode((800,800))
+font.init()
 
+# --- Loading Assets ----------------
+background = image.load("Assets/background.png")
+titleFont = font.SysFont("Comic Sans MS", 60)
+buttonFont = font.SysFont("Comic Sans MS", 48)
+# -----------------------------------
+def introScreen():
+    screen.blit(background, (-5,0))
+    titleText = "COVID-19 Education"
+    buttonText = "Click here to start!"
+    
+    title = titleFont.render(titleText, True, (0,0,0))
+    button = buttonFont.render(buttonText, True, (0,0,0))
+
+    screen.blit(title, (200,200))
+    screen.blit(button, (250, 700))
+    
+    display.flip()
 
 running = True
 myClock = time.Clock()
@@ -16,11 +34,13 @@ while running:
     for evnt in event.get():
         if evnt.type == QUIT:
             running = False
-        
-    boy.moveBoy(boy)
-    boy.drawBoy()
+
+    introScreen()
+    
+    #boy.moveBoy(boy)
+    #boy.drawBoy()
     #boy.drawHitbox()
-    boy.update()
+    #boy.update()
     myClock.tick(50)
     
 quit()
